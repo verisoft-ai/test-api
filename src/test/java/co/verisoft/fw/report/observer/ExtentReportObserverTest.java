8a3d02133652bdb5d24a2b8ea73a93917224b1ac
Nir Gallner <nir@verisoft.ai>
2023-05-25 00:00:00 +0300

    7

--- src/test/java/co/verisoft/fw/report/observer/ExtentReportObserverTest.java
+++ src/test/java/co/verisoft/fw/report/observer/ExtentReportObserverTest.java
@@ -76,7 +76,7 @@ public class ExtentReportObserverTest extends BaseTest {
     }
 
     @Test
-    @Disabled
+    //@Disabled
     public void shouldattachScreenShotToStep() throws AWTException, IOException {
         Robot robot = new Robot();
         BufferedImage screenShot = robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
